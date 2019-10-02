2b1e59e54cd6f7d04ed495d5689481e432b3d3ed
Alessandro Alberga <ale.alberga1@gmail.com>
2019-10-02 11:34:35 +1000

    Initial

---
+++ MK-1.py
@@ -0,0 +1,11 @@
+"""
+  @author Alessandro Alberga
+  @description MK-1 program entry point.
+"""
+
+# Primary program variable.
+run_program = True
+
+while (run_program):
+    print('MK-1')
+    run_program = False
